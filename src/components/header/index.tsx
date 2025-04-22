c52b0c18fda8b21e61e0427714f816741b056f96
YanGraim <yangraim@gmail.com>
2025-04-22 08:59:47 -0300

    loga usuario com o google

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,9 @@
 import styles from "@/components/header/Header.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 
 export function Header() {
+  const { data: session, status } = useSession()
   return (
     <header className={styles.header}>
       <section className={styles.content}>
@@ -15,7 +17,13 @@ export function Header() {
             Meu Painel
           </Link>
         </nav>
-        <button className={styles.loginButton}>Acessar</button>
+        {status === "loading" ? (
+          <></>
+        ) : session ? (
+          <button className={styles.loginButton} onClick={() => signOut()}>Olá, {session?.user?.name}</button>
+        ) : (
+          <button className={styles.loginButton} onClick={() => signIn("google")}>Acessar</button>
+        )}
       </section>
     </header>
   );
