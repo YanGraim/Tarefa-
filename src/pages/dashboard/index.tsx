2ad784e7512e7348e8610633178a7f10df90b799
YanGraim <yangraim@gmail.com>
2025-04-22 09:57:56 -0300

    estrutura e estiliza area de texto no painel do usuario

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from "next";
 import styles from "@/pages/dashboard/Dashboard.module.css";
 import Head from "next/head";
 import { getSession } from "next-auth/react";
+import { TextArea } from "@/components/textarea";
 
 export default function Dashboard() {
   return (
@@ -10,7 +11,22 @@ export default function Dashboard() {
         <title>Meu painel de tarefas</title>
       </Head>
 
-      <h1>Pagina painel</h1>
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
+
+            <form>
+              <TextArea placeholder="Digite sua tarefa..." />
+              <div className={styles.checkBoxArea}>
+                <input type="checkbox" className={styles.checkbox} />
+                <label>Deixar tarefa publica?</label>
+              </div>
+            </form>
+            <button type="submit" className={styles.button}>Registrar</button>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
